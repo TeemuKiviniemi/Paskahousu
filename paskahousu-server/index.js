4cc6db12f47c54349e84a07bd79299daa71030bb
Teemu Kiviniemi <kiviniemi.te@gmail.com>
2021-05-09 18:22:03 +0300

    small fixes

--- paskahousu-server/index.js
+++ paskahousu-server/index.js
@@ -33,18 +33,19 @@ io.on("connection", (socket) => {
 				const { data } = await axios.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
 				turn.push({ room: state.room, turn: 0, deckId: data.deck_id });
 				state.deckId = data.deck_id;
+				state.remaining = 49;
 				io.to(socket.id).emit("turn", true);
 				io.to(socket.id).emit("onStart", { deckId: data.deck_id });
 			} else {
 				objIndex = turn.findIndex((i) => i.room === state.room);
 				state.deckId = turn[objIndex].deckId;
+				state.remaining = 46;
 				io.to(socket.id).emit("turn", false);
 				io.to(socket.id).emit("onStart", { deckId: turn[objIndex].deckId });
 			}
 
 			io.to(state.room).emit("players", players);
 			io.to(state.room).emit("log", `${username} liittyi peliin`);
-			// console.log(user);
 
 			io.to(state.room).emit("updateGame", state);
 		}
@@ -52,7 +53,6 @@ io.on("connection", (socket) => {
 
 	socket.on("turn", (data) => {
 		roomIndex = turn.findIndex((i) => i.room === data.room);
-		console.log(data);
 		turn[roomIndex].turn === data.playerAmount - 1 ? (turn[roomIndex].turn = 0) : (turn[roomIndex].turn += 1);
 
 		playersInRoom = players.filter((p) => p.room === data.room);
@@ -70,13 +70,12 @@ io.on("connection", (socket) => {
 	});
 
 	socket.on("updateGame", (gameState) => {
-		console.log("UPDATE", gameState.stack);
+		console.log(gameState);
 		io.to(gameState.room).emit("updateGame", gameState);
 	});
 
 	socket.on("disconnect", () => {
 		players = players.filter((u) => u.id !== socket.id);
-		// console.log(players);
 
 		if (players.length === 0) {
 			turn = [];
